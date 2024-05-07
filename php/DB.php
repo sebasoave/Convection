114fb150d58f256d1e1cdc9252abf9147dac2792
sebasoave <122523060+sebasoave@users.noreply.github.com>
2024-05-07 15:48:20 +0200

    Partecipante
    update a partecipante

--- php/DB.php
+++ php/DB.php
@@ -9,17 +9,10 @@ class Database {
 
 	public static function connect() {
 		self::$connection = new mysqli(self::$host, self::$username, self::$password,self::$dbname);
-		if (self::$connection->connect_error) {
-			die("Connessione fallita: " . self::$connection->connect_error);
-			return false;
-		}
 		return true;
 	}
 	public  static function executeQuery($query) {
 		$result = self::$connection->query($query);
-		if (!$result) {
-			echo "Errore nella query: " . self::$connection->error;
-		}
 		return $result;
 	}
 
--- php/Partecipante.php
+++ php/Partecipante.php
@@ -1,9 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Partecipanti</title>
+</head>
+<body>
 <?php
 include "DB.php";
 include "Conf.php";
 Database::connect();
 $ris=Database::executeQuery("SELECT Programma.IdProgramma,Speech.IdSpeech,Speech.Titolo,Speech.Argomento,Programma.NomeSala,Programma.FasciaOraria,Sala.NpostiSala FROM `Programma` , `Speech`,`Sala` WHERE Programma.IdSpeech = Speech.IdSpeech AND Programma.NomeSala = Sala.NomeSala; ");
 ?>
+<h1>Programmi</h1>
 <table border="2">
     <tr>
         <th>IdProgramma</th>
@@ -51,41 +60,8 @@ for ($i=0; $i < $ris->num_rows; $i++) {
     </form></th>";
     echo "</tr>";
 }
-$par=Database::executeQuery("SELECT * FROM Seglie where IdPar = '".$_SESSION["user"][0]["IdPar"]."';");
-if ($par->num_rows > 0) {?>
-
-<table border="2">
-    <tr>
-        <th>IdProgramma</th>
-        <th>IdSpeech</th>
-        <th>Titolo	</th>
-        <th>Argomento	</th>
-        <th>NomeSala	</th>
-        <th>FasciaOraria</th>
-    </tr>
-<?php
-    for ($i=0; $i < $par->num_rows; $i++) { 
-        $idp=$par->fetch_assoc()["IdPar"];
-        $QueryForTable=Database::executeQuery("SELECT Programma.IdProgramma,Speech.IdSpeech,Speech.Titolo,Speech.Argomento,Programma.NomeSala,Programma.FasciaOraria FROM `Programma` , `Speech` WHERE Programma.IdSpeech = Speech.IdSpeech AND (Programma.IdProgramma =".$idp."); ");
-
-        for ($i=0; $i < $QueryForTable->num_rows; $i++) { 
-        echo "<tr>";
-        foreach ($QueryForTable->fetch_assoc() as $key => $value) { 
-            if ($key == "NomeSala") {
-                $nms=$value;
-            }
-            echo "<th>".$value."</th>";   
-        }
-        echo "</tr>";
-        }
-    }
-}
-?>
-</table>
-
-<?php
-
-
 Database::disconnect();
 ?>
-</table>
+    
+</body>
+</html>
